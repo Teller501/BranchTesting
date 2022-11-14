02ef8cb35f8a37c3de9c3006f8101290d4933683
Teller501 <ande777j@stud.kea.dk>
2022-11-14 14:32:46 +0100

    implemented mood feature

--- src/Cat.java
+++ src/Cat.java
@@ -9,4 +9,13 @@ public class Cat extends Animal{
     public boolean eat(String foodType) {
         return true;
     }
+
+    @Override
+    public void showMood(boolean mood) {
+        if (mood){
+            System.out.println("spinder");
+        }else {
+            System.out.println("hvæser");
+        }
+    }
 }
